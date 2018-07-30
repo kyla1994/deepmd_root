57c1eb0e176494e27fcb0a1f85d6eb758e2a6f20
王文惠 <wangwenhui@wangwenhuideMacBook-Pro.local>
2018-07-30 13:24:25 +0800

    change structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 setup(name='deepmd',
 version='1.2',
 author='Deepmd Group',
-packages=find_packages('src'),
+packages=find_packages(),
 package_data={
     '':['*.so', 'dp_*'],
 },
@@ -10,7 +10,7 @@ entry_points = {
     'console_scripts':[
         'dp_train = bin.dp_train:_main',
         'dp_test = bin.dp_test:_main',
-        'dp_frz = bin.dp_frz:_main', 
+        'dp_frz = bin.dp_frz:_main' 
     ]
 }
 )
